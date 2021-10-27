7554eba1f481985246e384f40dd5fdb3985b5657
ansur <ansurtm@gmail.com>
2021-10-27 19:09:31 +0900

    pm 7:09 서버고쳐서 다시 커밋시도

--- src/main/java/com/example/myapp/HomeController.java
+++ src/main/java/com/example/myapp/HomeController.java
@@ -1,39 +1,17 @@
 package com.example.myapp;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
+	@GetMapping(value = { "/", "/index" }) // GET요청을 받을때(서블릿과 유사)
+	public String home() {
+
+		System.out.println("home() 호출됨"); // 컨트롤러를 거치는지 확인을 위해 추가
+
+		return "home"; // redirect 필요시 [return "redirect:/index";]
+	} // home
 	
 }
